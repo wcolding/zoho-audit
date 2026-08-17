11920f70b7c1c51c283aa225c692608af4de86fd
wcolding <william.colding@gmail.com>
2026-08-17 09:57:48 -0400

    Use a class to match fields and enable email flag to output a report from email matching

---
+++ Matcher.py
@@ -0,0 +1,45 @@
+class Matcher():
+    master_dict = dict()
+    matched = dict()
+    name = "Generic"
+
+    def __init__(self):
+        self.fields = list()
+
+    def find_matches(self, contacts: list):
+        assert len(self.fields) > 0, "No search fields defined! Create a new class that inherits from Matcher and override __init__ to define some"
+        for contact in contacts:
+                new_entries = dict()
+                for field in self.fields:
+                    new_key = contact[field]
+                    if new_key not in new_entries.keys() and new_key.strip():
+                        new_entries[new_key] = contact
+                
+                for new_entry in new_entries:
+                    if new_entry in self.master_dict.keys():
+                        if new_entry not in self.matched.keys():
+                            self.matched[new_entry] = [self.master_dict[new_entry], new_entries[new_entry]]
+                        else:
+                            self.matched[new_entry].append(new_entries[new_entry])
+        
+                self.master_dict |= new_entries
+
+    def get_report(self) -> str:
+        num_matches = len(self.matched.keys())
+        if num_matches < 1:
+             return f"No duplicate entries found with {self.name} matcher using fields {self.fields}"
+
+        report = f"{num_matches} matching entries have been found with {self.name} Matcher:\n\n"
+        
+        for match in self.matched.keys():
+            report += f"{match} - {len(self.matched[match])} hits\n"
+            for entry in self.matched[match]:
+                report += f"    Name: {entry['Display Name']} | MASID: {entry['CF.MASID']} | Zoho ID: {entry['Contact Address ID']}\n"
+            report += "--------------------\n"
+
+        return report
+
+class EmailMatcher(Matcher):
+    def __init__(self):
+        self.fields = ['EmailID', 'CF.Accounting Email']
+        self.name = "Email"
--- main.py
+++ main.py
@@ -1,42 +1,15 @@
 import argparse
 import csv
+from Matcher import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--files', nargs='*', help='File(s) to include')
-parser.add_argument('-e', '--email', help='Check contacts for overlapping emails')
+parser.add_argument('-e', '--email', action="store_true", help='Check contacts for overlapping emails')
 args = parser.parse_args()
 
 master_list = list()
 
-def MatchEmails(contacts: list):
-    emails = dict()
-    matched = dict()
-    fields = ['EmailID', 'CF.Accounting Email']
-    for contact in contacts:
-        new_emails = dict()
-        for field in fields:
-            contact_email = contact[field]
-            if contact_email not in new_emails.keys() and contact_email.strip():
-                new_emails[contact_email] = contact
-        
-        for new_email in new_emails:
-            if new_email in emails.keys():
-                if new_email not in matched.keys():
-                    matched[new_email] = [emails[new_email], new_emails[new_email]]
-                else:
-                    matched[new_email].append(new_emails[new_email])
-
-        emails |= new_emails
-            
-    print("The following emails were matched:\n")
-    for match in matched.keys():
-        print(f"  {match} - {len(matched[match])} hits")
-        for entry in matched[match]:
-            print(f"    Name: {entry['Display Name']} | MASID: {entry['CF.MASID']} | Zoho ID: {entry['Contact Address ID']}")
-        print("--------------------")
-        
-
-for file in args.__dict__['files']:
+for file in args.files:
     print(f'File requested: {file}')
     file = open(file, "r")
     file_dict = csv.DictReader(file)
@@ -44,4 +17,10 @@ for file in args.__dict__['files']:
         master_list.append(row)
     file.close()
 
-#MatchEmails(master_list[0:100])
+if args.email:
+    email_matcher = EmailMatcher()
+    email_matcher.find_matches(master_list)
+    report = email_matcher.get_report()
+    report_file = open("report.txt", "w+")
+    report_file.write(report)
+    report_file.close()
