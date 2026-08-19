7a8e20d8ae8b163404f4263379da9ccb55974417
wcolding <william.colding@gmail.com>
2026-08-19 11:22:11 -0400

    Refactor HTML build to support multiple reports TODO: Make the buttons show/hide content

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import argparse
 import csv
 from matcher.Matcher import *
-from report import GenerateReportHTML
+from report import generate_report_HTML
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--files', nargs='*', help='File(s) to include')
@@ -20,27 +20,20 @@ for file in args.files:
         master_list.append(row)
     file.close()
 
+email_matcher = None
+phone_matcher = None
+name_matcher = None
+
 if args.email:
     email_matcher = EmailMatcher()
     email_matcher.find_matches(master_list)
-    GenerateReportHTML(email_matcher.matched)
-    
-
 
 if args.phone:
     phone_matcher = PhoneMatcher()
     phone_matcher.find_matches(master_list)
-    report = phone_matcher.get_report()
-    report_file = open("phone_report.txt", "w+")
-    report_file.write(report)
-    report_file.close()
-    print("Wrote report to 'phone_report.txt'")
 
 if args.name:
     name_matcher = NameMatcher()
     name_matcher.find_matches(master_list)
-    report = name_matcher.get_report()
-    report_file = open("name_report.txt", "w+")
-    report_file.write(report)
-    report_file.close()
-    print("Wrote report to 'name_report.txt'")
+
+generate_report_HTML(email_matcher, phone_matcher, name_matcher)
--- report.py
+++ report.py
@@ -1,4 +1,39 @@
-def GenerateReportHTML(matches: dict):
+from matcher.Matcher import *
+
+def get_report_row(template: str, entry: dict) -> str:
+    row = template.replace("%%ENTRY_DISPLAY_NAME%%", entry['Display Name'])
+    row = row.replace("%%ENTRY_MAS_ID%%", entry['CF.MASID'])
+    row = row.replace("%%ENTRY_ZOHO_ID%%", entry['Contact Address ID'])
+    return row
+
+def get_group_report(template: str, group: dict) -> str:
+    group_report_full = template.replace("%%REPORT_HEADER%%", f"{group[0]} - {len(group[1])} hits")
+    group_report_sections = group_report_full.split("%%REPORT_ENTRY%%")
+    group_report = group_report_sections[0]
+    for matched_entry in group[1]:
+        cur_report_row = get_report_row(group_report_sections[1], matched_entry)
+        group_report += cur_report_row
+    group_report += group_report_sections[2]
+    return group_report
+
+def get_report_content(template: str, matcher: Matcher) -> str:
+    sections = template.split("%%REPORT_SECTION%%")
+    pre_reports_section = sections[0].replace("%%REPORT_CONTENT_ID%%", matcher.name) \
+        .replace("%%REPORT_CONTENT_HEADER%%", f"By {matcher.name} - {len(matcher.matched.keys())} results")
+    reports_section = sections[1]
+    post_reports_sections = sections[2]
+
+    content = pre_reports_section
+
+    for group in matcher.matched.items():
+        cur_group_report = get_group_report(reports_section, group)
+        content += cur_group_report
+
+    content += post_reports_sections
+    return content
+
+
+def generate_report_HTML(email_matcher: EmailMatcher, phone_matcher: PhoneMatcher, name_matcher:NameMatcher):
     html_template = open("report_template.html", "r")
     html_data = html_template.read()
     html_template.close()
@@ -8,26 +43,27 @@ def GenerateReportHTML(matches: dict):
     css_file.close()
     html_data = html_data.replace("%%STYLE_SECTION%%", css_data)
 
-    template_sections = html_data.split("%%REPORT_SECTION%%")
-    pre_report_section = template_sections[0]
-    report_section = template_sections[1]
-    post_report_section = template_sections[2]
+    template_sections = html_data.split("%%REPORT_CONTENT_SECTION%%")
+    pre_content_section = template_sections[0]
+    content_section = template_sections[1]
+    post_content_section = template_sections[2]
 
     output_file = open("report.html", "w+")
-    output_file.write(pre_report_section)
+    output_file.write(pre_content_section)
 
-    for match_group in matches.keys():
-        cur_report = report_section.replace("%%REPORT_HEADER%%", f"{match_group} - {len(matches[match_group])} hits")
-        cur_report_sections = cur_report.split("%%REPORT_ENTRY%%")
-        output_file.write(cur_report_sections[0])
-        for matched_entry in matches[match_group]:
-            cur_report_row = cur_report_sections[1].replace("%%ENTRY_DISPLAY_NAME%%", matched_entry['Display Name'])
-            cur_report_row = cur_report_row.replace("%%ENTRY_MAS_ID%%", matched_entry['CF.MASID'])
-            cur_report_row = cur_report_row.replace("%%ENTRY_ZOHO_ID%%", matched_entry['Contact Address ID'])
-            output_file.write(cur_report_row)
-        output_file.write(cur_report_sections[2])
+    if email_matcher:
+        content = get_report_content(content_section, email_matcher)
+        output_file.write(content)
 
-    output_file.write(post_report_section)
+    if phone_matcher:
+        content = get_report_content(content_section, phone_matcher)
+        output_file.write(content)
+
+    if name_matcher:
+        content = get_report_content(content_section, name_matcher)
+        output_file.write(content)
+
+    output_file.write(post_content_section)
     output_file.close()
         
 
